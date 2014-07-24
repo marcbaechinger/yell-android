ad920632bd9325cb1600e37efa77f9323a987fc8
marcbaechinger <marc.baechinger@gmail.com>
2014-07-24 03:27:44 +0200

    simplified by removing redundant code

--- app/src/main/java/yellcast/com/yell/activities/CreateYellActivity.java
+++ app/src/main/java/yellcast/com/yell/activities/CreateYellActivity.java
@@ -3,7 +3,6 @@ package yellcast.com.yell.activities;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
 
 import yellcast.com.yell.R;
 import yellcast.com.yell.fragments.YellNodeForm;
@@ -28,14 +27,4 @@ public class CreateYellActivity extends Activity {
         getMenuInflater().inflate(R.menu.create_yell, menu);
         return true;
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        return super.onOptionsItemSelected(item);
-    }
-
 }
