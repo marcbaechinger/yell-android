ed9fd8888a8ca601ef2ede2919d186ac0a11768b
marcbaechinger <marc.baechinger@gmail.com>
2014-07-24 03:09:43 +0200

    cleanup

--- app/src/main/java/yellcast/com/cast/ReceiverApplicationListener.java
+++ app/src/main/java/yellcast/com/cast/ReceiverApplicationListener.java
@@ -2,9 +2,6 @@ package yellcast.com.cast;
 
 import static com.google.android.gms.cast.Cast.Listener;
 
-/**
- * Created by marcbaechinger on 21.07.14.
- */
 public class ReceiverApplicationListener extends Listener {
 
     @Override
